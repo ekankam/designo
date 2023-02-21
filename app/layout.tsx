184e118f211945792a1a88909f6aa7d3d65145a8
ekankam <eb.kankam26@gmail.com>
2023-02-20 20:28:26 -0600

    setup custom fonts and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
-import { Jost } from '@next/font/google'
-import './globals.css'
+import { Jost } from '@next/font/google';
+import './globals.css';
 
-
-const jost = Jost({ subsets: ['latin'] })
-
+const jost = Jost({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -17,7 +15,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        <div className="max-w-[1440px] min-w-[375px] w-full min-h-screen bg-primary-white text-16">
+          <p>Navigation</p>
+          <main>{children}</main>
+          <footer>Footer</footer>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-peach': 'rgba(231, 129, 107, 1)',
+        'light-peach': 'rgba(255, 173, 155, 1)',
+        'primary-black': 'rgba(29, 28, 30, 1)',
+        'dark-grey': 'rgba(51, 49, 54, 1)',
+        'primary-white': 'rgba(255, 255, 255, 1)',
+        'light-grey': 'rgba(223, 223, 223, 1)',
+      },
+      fontSize: {
+        'text-48': ['48px', '48px'],
+        'text-40': ['40px', '48px'],
+        'text-20': ['20px', '26px'],
+        'text-16': ['16px', '26px'],
+      },
+    },
   },
   plugins: [],
-}
+};
